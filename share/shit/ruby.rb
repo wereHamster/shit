eabaf2d24ee1ef88a84e2962f9a986970c41aff4
Tomas Carnecky <tom@dbservice.com>
2010-10-20 00:22:18 +0200

    Ruby wants make and make install separately

--- share/shit/ruby.rb
+++ share/shit/ruby.rb
@@ -1,6 +1,6 @@
 
 poop 'ruby', '1.9.2-p0' do
   unpack "http://ftp.ruby-lang.org//pub/ruby/1.9/ruby-#{version}.tar.gz" do
-    configure and make 'install'
+    configure and make and make :install
   end
 end
